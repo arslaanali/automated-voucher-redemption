de0791b06c46ec458a1176a6b9a0f5d4d9cb6497
Arslaan <arslan.ali@i-ways.net>
2021-04-02 13:50:52 +0500

    cleaned branch

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div style="background:black">
-        <h1 class="text-center text-white"  > Your {{ auth()->user()->email }}</h1>
-    </div>
-
-    <home></home>
-@endsection
